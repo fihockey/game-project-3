517dae3418a43bcdcd3f39f1c5c970bc15808ce2
fihockey <fiona.hockey98@gmail.com>
2023-12-07 18:37:03 +0000

    Modified Index to group my answer buttons together. Edited the data, but changed it back. Added the variables to the main.scss file. Edited the main.ts file to allow the answer buttons to turn green and red of they are wrong or right.

--- src/data.ts
+++ src/data.ts
@@ -8,37 +8,37 @@ export const quizQuestions: question[] = [
   {
     question: "Who was the first presenter of the absolute hit television show Deal or No Deal?",
     answers: ["Dwain The Rock Johnson", "Mary Berry", "Noel Edmunds", "Mr Blobby"],
-    correctAnswer: 3,
+    correctAnswer: 2,
   },
 
   {
     question: "When would you use a Call Back Function?",
   answers: ["To loop through an array", "To execute a function that has been previously defined", "To break out of a function", "To try and get back with your ex"],
-  correctAnswer: 2, 
+  correctAnswer: 1, 
   },
 
   {
     question: "Why do we use Query Selectors?",
   answers: ["To access HTML elements", "To test an individual function", "To stop errors on the type", "To ask JSON when he is having a super lit party"],
-  correctAnswer: 1, 
+  correctAnswer: 0, 
   },
 
   {
     question: "What does D.R.Y stand for in terms of functions?",
   answers: ["Don't Recognise Yourself", "Deliciously Roasted Yams", "Delightfully Random Yeti", "Don't Repeat Yourself"],
-  correctAnswer: 4, 
+  correctAnswer: 3, 
   },
 
   {
     question: "Why do we use Event Listeners?",
   answers: ["To get notified when the new verion of Node has dropped", "To get access to object values and variables", "To execute a function once an event has happened", "To spy on Winnie The Pooh organising a top secret honey raid"],
-  correctAnswer: 3, 
+  correctAnswer: 2, 
   },
 
   {
     question: "Who is the greatest quiz developer this world has ever seen?",
   answers: ["Fiona", "Fi", "Fifi", "The Hockinator"],
-  correctAnswer: 4, 
+  correctAnswer: 3, 
   },
 
 //   {
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import "./main.scss";
 import { quizQuestions, question } from "./data.ts";
 
 let currentQuestion = 0;
@@ -5,17 +6,19 @@ let currentQuestion = 0;
 const questionContainer =
   document.querySelector<HTMLParagraphElement>("#question");
 const answerContainerOne =
-  document.querySelector<HTMLParagraphElement>("#answer-one");
+  document.querySelector<HTMLButtonElement>("#answer-one");
 const answerContainerTwo =
-  document.querySelector<HTMLParagraphElement>("#answer-two");
+  document.querySelector<HTMLButtonElement>("#answer-two");
 const answerContainerThree =
-  document.querySelector<HTMLParagraphElement>("#answer-three");
+  document.querySelector<HTMLButtonElement>("#answer-three");
 const answerContainerFour =
-  document.querySelector<HTMLParagraphElement>("#answer-four");
-const nextQuestionButton = 
+  document.querySelector<HTMLButtonElement>("#answer-four");
+const answerButtons =
+  document.querySelectorAll<HTMLButtonElement>(".answer-buttons")
+const nextQuestionButton =
   document.querySelector<HTMLButtonElement>("#next-question");
 const startAgainButton = 
-  document.querySelector<HTMLBRElement>("#start-again")
+  document.querySelector<HTMLButtonElement>("#start-again");
 
 if (
   !questionContainer ||
@@ -33,58 +36,80 @@ if (
 // ACCESS QUESTION THROUGH INDEX
 // ADD DISPLAY FUNCTION
 
-const displayQuestion = (currentQuestion : number) => {
-  const question = quizQuestions [currentQuestion].question;
-  questionContainer.innerHTML = question
-}
+const displayQuestion = (currentQuestion: number) => {
+  const question = quizQuestions[currentQuestion].question;
+  questionContainer.innerHTML = question;
+};
 
-displayQuestion(currentQuestion)
+displayQuestion(currentQuestion);
 
 // ANSWERS ON THE SCREEN
-const displayAnswers = (currentQuestion : number) => {
-  const answerOne = quizQuestions [currentQuestion].answers[0];
+const displayAnswers = (currentQuestion: number) => {
+  const answerOne = quizQuestions[currentQuestion].answers[0];
   answerContainerOne.innerHTML = answerOne;
-  const answerTwo = quizQuestions [currentQuestion].answers[1];
+  const answerTwo = quizQuestions[currentQuestion].answers[1];
   answerContainerTwo.innerHTML = answerTwo;
-  const answerThree = quizQuestions [currentQuestion].answers[2];
+  const answerThree = quizQuestions[currentQuestion].answers[2];
   answerContainerThree.innerHTML = answerThree;
-  const answerFour = quizQuestions [currentQuestion].answers[3];
+  const answerFour = quizQuestions[currentQuestion].answers[3];
   answerContainerFour.innerHTML = answerFour;
-}
+};
 
-displayAnswers(currentQuestion)
-
-
-
-
+displayAnswers(currentQuestion);
 
 // ANSWERS NEED TO BE CLCIKABLE AND SHOW UP RIGHT OR WRONG (turn green/red)
 
+const handleCorrectAnswer = (event: Event, question: question) => {
+  const button = event.target as HTMLButtonElement;
+  let correctAnswer = question.answers[question.correctAnswer];
+  if (button.innerText === correctAnswer) {
+    turnButtonGreen(button);
+    // EXECUTE FUNCTION THAT MOVES THE PROGRESS BAR ON
+  } else {
+    turnButtonRed(button);
+  }
+};
+
+const turnButtonGreen = (button: HTMLButtonElement) => {
+  button.style.backgroundColor = "green";
+};
+
+const turnButtonRed = (button: HTMLButtonElement) => {
+  button.style.backgroundColor = "red";
+};
+
+
+answerButtons.forEach((button) => {
+  button.addEventListener("click", (event) => handleCorrectAnswer(event, quizQuestions[currentQuestion]));
+});
+
+
+// answerButtons.forEach((button) => {
+//   button.addEventListener("click", handleCorrectAnswer);
+// });
+
 // WHEN THE ANSWER IS RIGHT THE SEGMENTS IN THE PROGRESS BAR NEED TO LIGHT UP BY ONE
 // WHEN THE ANSWER IF WRONG THE SEGMENTS NEED TO STAY THE SAME
-// USE IF / ELSE IF STANTEMENTS TO ORGANISE RIGHT OR WRONG ANSWERS. E.G. THIS IS 
-
-
+// USE IF / ELSE IF STANTEMENTS TO ORGANISE RIGHT OR WRONG ANSWERS. E.G. THIS IS
 
 // NEXT BUTTON TO GO TO THE NEXT QUESTION
 //(MOVE TO THE NEXT INDEX/QUESTION EVERYTIME THE NEXT ARROW IS CLICKED)
 // HAVE THIS BUTTON HAVE A COUNT TO UPDATE THE QUESTION NUMBER BY 1.
 
-const handleIncrementQuestion = (event : Event) => {
+const handleIncrementQuestion = (event: Event) => {
   event.target as HTMLButtonElement;
- currentQuestion++ 
- displayQuestion(currentQuestion)
- displayAnswers(currentQuestion)
-}
+  currentQuestion++;
+  displayQuestion(currentQuestion);
+  displayAnswers(currentQuestion);
+};
 
 nextQuestionButton.addEventListener("click", handleIncrementQuestion);
 
-
 // BUTTON THAT TAKES YOU BACK TO THE START OF THE QUIZ
 
-const handleStartAgain = (event : Event) => {
+const handleStartAgain = (event: Event) => {
   event.target as HTMLButtonElement;
-  window.location.reload()
-}
+  window.location.reload();
+};
 
-startAgainButton.addEventListener("click", handleStartAgain)
+startAgainButton.addEventListener("click", handleStartAgain);
